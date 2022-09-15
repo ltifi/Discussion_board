4c94a2a0003709af405effcd9abca151c5f6f2a4
sedki.ltifi <sedki.ltifi@ensi-uma.tn>
2022-09-15 17:15:59 +0100

    create board home vue

--- boards/views.py
+++ boards/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Board
 # Create your views here.
 
 def home (request):
-    return HttpResponse('Hello, my name is sedki, this is my first website')
+    boards=Board.objects.all()
+    # boards_names=[]
+    # for board in boards:
+    #     boards_names.append(board.name)
+    # print(boards_names)
+    # responce_html='<br>'.join(boards_names)
+    # return HttpResponse(responce_html)
+    return render(request,'home.html',{'boards':boards})
+    

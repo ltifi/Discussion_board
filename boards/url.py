aba97898434f174ddd0b936f806fc7273deac3b1
sedki.ltifi <sedki.ltifi@ensi-uma.tn>
2022-09-16 14:18:58 +0100

    refactor html pages

---
+++ boards/url.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from boards import views
+import boards
+urlpatterns = [
+    path('', views.home, name='index'),
+    path('about/', views.about, name='about'),
+    path('boards/<int:board_id>/', views.board_topics, name='board_topics')]

c06345bf7c7f80d5934d64a7bc007593891489d4
ltifi <ltifisedkiinfo@gmail.com>
2022-09-25 12:27:38 +0100

    user login

--- accounts/url.py
+++ accounts/url.py
@@ -1,7 +1,9 @@
+from re import template
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('logout/',auth_views.LogoutView.as_view(),name='logout')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'),name='login')
 ]
--- discussion_board/settings.py
+++ discussion_board/settings.py
@@ -131,3 +131,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGOUT_REDIRECT_URL='home'
+LOGIN_REDIRECT_URL='home'

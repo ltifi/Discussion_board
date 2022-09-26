cad690068d8f597f4017a12054a208e739c853c6
ltifi <ltifisedkiinfo@gmail.com>
2022-09-26 12:35:29 +0100

    reply posts and calc numeric attributes

--- boards/models.py
+++ boards/models.py
@@ -1,7 +1,6 @@
-from email import message
-from statistics import mode
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import Truncator
 # Create your models here.
 
 
@@ -16,7 +15,7 @@ class Board(models.Model):
        return Post.objects.filter(topic__board=self).count()
 
     def get_last_post(self):
-        return Post.objects.filter(topic__board=self).order_by('-created_dt').firs
+        return Post.objects.filter(topic__board=self).order_by('-created_dt').first()
 
 
 class Topic(models.Model):
@@ -38,3 +37,6 @@ class Post(models.Model):
     created_by = models.ForeignKey(
         User, related_name='posts', on_delete=models.CASCADE)
     created_dt = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        truncted_message = Truncator(self.message)
+        return truncted_message.chars(30)

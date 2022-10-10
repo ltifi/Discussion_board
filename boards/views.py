337a0ea463162c279e2597e98e214cdd1628894a
ltifi <ltifisedkiinfo@gmail.com>
2022-10-10 11:11:40 +0100

    add pagination

--- boards/views.py
+++ boards/views.py
@@ -10,28 +10,39 @@ from django.db.models import Count
 from django.views.generic import UpdateView, ListView
 from django.utils import timezone
 from django.utils.decorators import method_decorator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
 
 
 # def home(request):
 #     boards = Board.objects.all()
-    # boards_names=[]
-    # for board in boards:
-    #     boards_names.append(board.name)
-    # print(boards_names)
-    # responce_html='<br>'.join(boards_names)
-    # return HttpResponse(responce_html)
-    # return render(request, "home.html", {"boards": boards})
-    
+# boards_names=[]
+# for board in boards:
+#     boards_names.append(board.name)
+# print(boards_names)
+# responce_html='<br>'.join(boards_names)
+# return HttpResponse(responce_html)
+# return render(request, "home.html", {"boards": boards})
+
+
 class BoardListView(ListView):
     model = Board
-    context_object_name = 'boards'
-    template_name = 'home.html'
+    context_object_name = "boards"
+    template_name = "home.html"
+
 
 def board_topics(request, board_id):
     board = get_object_or_404(Board, pk=board_id)
-    topics = board.topics.order_by("-created_dt").annotate(comments=Count("posts"))
+    queryset = board.topics.order_by("-created_dt").annotate(comments=Count("posts"))
+    page = request.GET.get("page", 1)
+    paginator = Paginator(queryset, 20)
+    try:
+        topics = paginator.page(page)
+    except PageNotAnInteger:
+        topics = paginator.page(1)
+    except EmptyPage:
+        topics = paginator.page(paginator.num_pages)
     return render(request, "topics.html", {"board": board, "topics": topics})
 
 
@@ -68,7 +79,7 @@ def new_topic(request, board_id):
 
 def topic_posts(request, board_id, topic_id):
     topic = get_object_or_404(Topic, board__pk=board_id, pk=topic_id)
-    topic.views+=1
+    topic.views += 1
     topic.save()
     return render(request, "topic_posts.html", {"topic": topic})
 
@@ -88,20 +99,24 @@ def reply_topic(request, board_id, topic_id):
         form = NewTopicForm()
     return render(request, "reply_topic.html", {"topic": topic, "form": form})
 
-@method_decorator(login_required,name='dispatch')
+
+@method_decorator(login_required, name="dispatch")
 class PostUpdateView(UpdateView):
     model = Post
-    fields = ('message',)
-    template_name = 'edit_post.html'
-    pk_url_kwarg = 'post_id'
-    context_object_name = 'post'
+    fields = ("message",)
+    template_name = "edit_post.html"
+    pk_url_kwarg = "post_id"
+    context_object_name = "post"
 
     def form_valid(self, form):
         post = form.save(commit=False)
         post.updated_by = self.request.user
         post.updated_dt = timezone.now()
         post.save()
-        return redirect('topic_posts',board_id=post.topic.board.pk,topic_id=post.topic.pk)
+        return redirect(
+            "topic_posts", board_id=post.topic.board.pk, topic_id=post.topic.pk
+        )
+
 
 def about(request):
     return HttpResponse(request, "yes")
